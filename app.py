c28c4a5c89fd055169848c5830185ceef48c91a5
its_Rohit <113571542+Rohitwagh1406@users.noreply.github.com>
2024-06-10 18:29:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, request, jsonify, render_template
+import requests
+import fitz  # PyMuPDF
+import PyPDF2
+import gemini
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/job_description', methods=['POST'])
+def generate_job_description():
+    if request.json:
+        data = request.json
+        job_title = data.get('job_title')
+        location = data.get('location')
+        mandatory_skills = data.get('mandatory_skills', [])
+        overall_experience = data.get('overall_experience')
+        work_type = data.get('work_type')
+        mode_of_work = data.get('mode_of_work')
+        education_requirement = data.get('education_requirement')
+        ctc = data.get('ctc')
+
+        if job_title and location and overall_experience and mandatory_skills and work_type and mode_of_work and education_requirement and ctc:
+            jd_text = gemini.generate_job_description(job_title, location, mandatory_skills, overall_experience, work_type, mode_of_work, education_requirement, ctc)
+            return jsonify({'job_description': jd_text}), 200
+        else:
+            return jsonify({'error': 'Missing required fields.'}), 400
+    else:
+        return jsonify({'error': 'Invalid request format.'}), 400
+
+
+@app.route('/pdf_job_description', methods=['POST'])
+def generate_job_description_from_pdf():
+    if 'pdf_file' in request.files:
+        pdf_file = request.files['pdf_file']
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        pdf_text = ''
+        for page_num in range(len(pdf_reader.pages)):
+            pdf_text += pdf_reader.pages[page_num].extract_text()
+        reformated_text = gemini.reformat_job_description(pdf_text)
+        return jsonify({'job_description': reformated_text}), 200
+    return jsonify({'error': 'PDF file not provided.'}), 400
+
+
+@app.route('/pdf_job_description_s3', methods=['POST'])
+def generate_job_description_from_url():
+    pdf_url = request.json.get("pdf_url")
+
+    try:
+        response = requests.get(pdf_url)
+        response.raise_for_status()
+        pdf_data = response.content
+
+        pdf_document = fitz.open(stream=pdf_data, filetype="pdf")
+
+        text = ""
+        for page_num in range(pdf_document.page_count):
+            page = pdf_document[page_num]
+            text += page.get_text()
+        reformated_text = gemini.reformat_job_description(text)
+        return jsonify({'job_description': reformated_text}), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ gemini.py
@@ -0,0 +1,61 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def post_process_text(text):
+    cleaned_text = text.replace("**", "")
+    return cleaned_text
+
+
+def generate_job_description(job_title, location, mandatory_skills, overall_experience, work_type, mode_of_work
+                             , education_requirement, ctc):
+    genai.configure(api_key= os.getenv("GEMINI_API_KEY"))
+    model = genai.GenerativeModel('gemini-pro')
+    jd = (f"Job Description for {job_title} at {location} if any\n Skills: {', '.join(mandatory_skills)}\n"
+          f"Experience: {overall_experience} years\n"
+          f"Work Type: {work_type}\n Mode of Work: {mode_of_work}"
+          f"The JD should be in the following format : \n"
+          f"Job title : {job_title}\n"
+          f"Summary : it should include the brief about the {job_title} the location of work {location} if and the "
+          f"type of work {work_type} and the mode of work {mode_of_work}\n"
+          f"Responsibilities/Duties: \n"
+          f"{education_requirement} and {overall_experience} and {mandatory_skills} and any other additional skills "
+          f"under Qualifications and skills: \n"
+          f"Mandatory Skills: {mandatory_skills}\n"
+          f"Education Requirements: {education_requirement}\n"
+          f"Experience: {overall_experience}\n"
+          f"CTC Range:{ctc}\n"
+          f"Equal Opportunity Employer:\nWe are proud to be an Equal Opportunity Employer with a global culture that "
+          f"embraces diversity. We are committed to providing an environment free of unfair discrimination and "
+          f"harassment. We do not discriminate based on age, race, colour, sex, religion, national origin, "
+          f"disability, pregnancy, marital status, sexual orientation, gender reassignment, veteran status, "
+          f"or other protected category.\n\n the end of the job description should always include the above Equal "
+          f"Opportunity Employer paragraph"
+          )
+    response = model.generate_content(jd)
+    return post_process_text(response.text)
+
+
+def reformat_job_description(extracted_text):
+    genai.configure(api_key= os.getenv("GEMINI_API_KEY"))
+    model = genai.GenerativeModel('gemini-pro')
+    prompt = (f'''Rewrite the JD in the format\n
+    Job title :\n
+    Summary :\n
+    Responsibilities/Duties :\n
+    Preferred Qualifications :\n
+    Mandatory Skills :\n
+    Education Requirements :\n
+    Experience :\n
+    CTC Range :\n
+    Equal Opportunity Employer:\nWe are proud to be an Equal Opportunity Employer with a global culture that
+    embraces diversity. We are committed to providing an environment free of unfair discrimination and
+    harassment. We do not discriminate based on age, race, colour, sex, religion, national origin,
+    disability, pregnancy, marital status, sexual orientation, gender reassignment, veteran status,
+    or other protected category.\n\n the end of the job description should always include the above Equal
+    Opportunity Employer paragraph if any of the above subheadings are not present in the JD then leave a empty space\n\n 
+    Here is the JD text {extracted_text}.''')
+    response = model.generate_content(prompt)
+    return post_process_text(response.text)
